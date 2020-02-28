e22e659c5dfe3b8fcf9f25708bc6babfc6e95a69
findus19 <ivanexs@mail.ru>
2020-02-28 20:38:17 +0300

    day25

--- script/script.js
+++ script/script.js
@@ -10,8 +10,8 @@ window.addEventListener('DOMContentLoaded', () =>{
     movies = document.querySelector('.heroe_movies'),
     photo = document.querySelector('.heroe_photo');
 
-  let cardItem = document.querySelectorAll('.card_item'),
-    content = document.querySelector('.card_content');
+  let cardItem = document.querySelectorAll('.card'),
+    content = document.querySelectorAll('.card_content');
 
 
   fetch('./dbHeroes.json')
@@ -21,21 +21,90 @@ window.addEventListener('DOMContentLoaded', () =>{
 
   const practice = (response) => {
     response.forEach((item) => {
-          cardItem.forEach((elem, index) => {
+      
+      createDiv(item);
+      
+        /* cardItem.forEach((elem, index) => {
+            console.log(elem[index])
           elem[index] = document.createElement('div');
           elem[index].classList.add('card_item');
-          cardItem[0].appendChild(elem[index]);
+          cardItem[index].appendChild(elem[index]);
           photo.src = item.photo;
+        })
+        content.forEach((elem, index) => {
+          elem[index] = document.createElement('div');
+          elem[index].classList.add('card_content');
+          content[index].appendChild(elem[index]);
           name.textContent = item.name;
           species.textContent = item.species;
-          gender.textContent = item.gender;
+          gender.textContent = item.genger;
           birthDay.textContent = item.birthDay;
           deathDay.textContent = item.deathDay;
           status.textContent = item.status;
           actors.textContent = item.actors;
-          movies.textContent = item.movies;
-      })
+          movies.textContent = item.movies; 
+        }) */
+    
     });
 
-  }
+  };
+
+  const createDiv = (item) => {
+    cardItem.forEach((elem, index) => {
+        console.log(elem[index])
+        elem[index] = document.createElement('div');
+        elem[index].classList.add('card_item');
+        //cardItem[0].appendChild(elem[index]);
+        cardItem[index].appendChild(elem[index]);
+        let img = document.createElement('img');
+        img.classList.add('heroe_photo');
+        elem[index].appendChild(img);
+        img.src = item.photo;
+        let newContent = document.createElement('div');
+        newContent.classList.add('card_content');
+        elem[index].appendChild(newContent);
+
+        let newName = document.createElement('div');
+        newName.classList.add('heroe_name');
+        newContent.appendChild(newName);
+        newName.textContent = item.name;
+
+        let newSpecies = document.createElement('div');
+        newSpecies.classList.add('heroe_species');
+        newContent.appendChild(newSpecies);
+        newSpecies.textContent = item.species;
+
+        let newGender = document.createElement('div');
+        newGender.classList.add('heroe_gender');
+        newContent.appendChild(newGender);
+        newGender.textContent = item.genger;
+
+        let birthDay = document.createElement('div');
+        birthDay.classList.add('birthDay');
+        newContent.appendChild(birthDay);
+        birthDay.textContent = item.birthDay;
+
+        let deathDay = document.createElement('div');
+        deathDay.classList.add('deathDay');
+        newContent.appendChild(deathDay);
+        deathDay.textContent = item.deathDay;
+
+        let newStatus = document.createElement('div');
+        newStatus.classList.add('heroe_status');
+        newContent.appendChild(newStatus);
+        newStatus.textContent = item.status;
+
+        let newActors = document.createElement('div');
+        newActors.classList.add('heroe_actors');
+        newContent.appendChild(newActors);
+        newActors.textContent = item.actors;
+
+        let newMovies= document.createElement('div');
+        newMovies.classList.add('heroe_movies');
+        newContent.appendChild(newMovies);
+        newMovies.textContent = item.movies; 
+    })
+  
+};
+
 })
